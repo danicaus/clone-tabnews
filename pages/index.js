a77c5e758a4d8b48f36407c70145af795b2ecb9b
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2024-11-26 02:04:37 +0000

    Cria primeira página

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Filipe, eu te amo! ❤️</h1>
+            <footer>
+                <small>Feito com ❤️ por Dani Caus. Código em desenvolvimento.</small>
+            </footer>
+        </>
+    )
+}
