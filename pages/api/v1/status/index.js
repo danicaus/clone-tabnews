db7a4e6f3ed2a0403d0cb8913fa7f3d9dcb585e3
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2025-02-12 01:55:52 +0000

    feat: adiciona config de database e variáveis ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
+import { query } from "../../../../infra/database";
+
+export default async function status(req, res) {
+  const result = await query("SELECT 1 + 1 as resultado_soma;");
+  console.log(result.rows);
   res.status(200).json({ chave: "valor" });
 }
