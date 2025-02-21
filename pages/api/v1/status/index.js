9f9cc95895c4ddbd0980857a637db3a42c789b53
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2025-02-21 03:05:21 +0000

    retorna dados reais no endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
 import { query } from "infra/database";
+import { client } from "infra/database";
 
 export default async function status(req, res) {
-  const result = await query("SELECT 1 + 1 as resultado_soma;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsQuery =
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;";
+  const databaseOpenedConnectionsResult = await query({
+    text: databaseOpenedConnectionsQuery,
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,34 @@
-test("GET to /api/v1/status SHOULD return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET to /api/v1/status", () => {
+  let response;
+  let responseBody;
 
-  expect(response.status).toBe(200);
+  beforeEach(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    responseBody = await response.json();
+  });
+
+  test("SHOULD return 200", () => {
+    expect(response.status).toBe(200);
+  });
+
+  test("SHOULD return correct updated_at", async () => {
+    const receivedUpdatedAt = responseBody.updated_at;
+    const parsedUpdateAt = new Date(receivedUpdatedAt).toISOString();
+    expect(receivedUpdatedAt).toBe(parsedUpdateAt);
+  });
+
+  test("SHOULD return postgres version", async () => {
+    const { database } = responseBody.dependencies;
+    expect(database.version).toBe("16.0");
+  });
+
+  test("SHOULD return max connections", () => {
+    const { database } = responseBody.dependencies;
+    expect(database.max_connections).toBe(100);
+  });
+
+  test("SHOULD return used connections", () => {
+    const { database } = responseBody.dependencies;
+    expect(database.opened_connections).toBe(1);
+  });
 });
