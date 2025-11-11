9b410df4acb2bc8efc980149f9a2ef662ab491f5
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2025-11-10 22:12:14 -0300

    ci: create post dev script

---
+++ infra/scripts/postdev-docker-stop.js
@@ -0,0 +1,26 @@
+const { spawn } = require('child_process');
+const { execSync } = require('child_process');
+
+const nextDev = spawn('next', ['dev'], {
+  stdio: 'inherit',
+  shell: true
+});
+
+function cleanup() {
+  console.log('\n🛑 Parando servidor de desenvolvimento...');
+  try {
+    execSync('npm run docker:stop:dev', { stdio: 'inherit' });
+  } catch (error) {
+    console.error('Erro ao parar containers:', error.message);
+  }
+  process.exit(0);
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
+
+nextDev.on('exit', (code) => {
+  if (code !== 0) {
+    cleanup();
+  }
+});
