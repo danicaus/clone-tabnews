c6fd2e0b9545e38350655d2c312da9b341474e79
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2025-11-10 23:14:18 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -5,31 +5,35 @@
 import orchestrator from "tests/integration/orchestrator.js";
 import database from "infra/database";
 
-describe("POST to /api/v1/migrations", () => {
-  let response;
-  let responseBody;
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Retrieving pending migrations", () => {
+      let response;
+      let responseBody;
 
-  beforeAll(async () => {
-    await orchestrator.waitForAllServices();
-    await database.query("drop schema public cascade; create schema public;");
-  });
+      beforeAll(async () => {
+        await orchestrator.waitForAllServices();
+        await database.query("drop schema public cascade; create schema public;");
+      });
 
-  beforeEach(async () => {
-    response = await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+      beforeEach(async () => {
+        response = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+        responseBody = await response.json();
+      });
+
+      test("For the first time", () => {
+        expect(response.status).toBe(201);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", () => {
+        expect(response.status).toBe(200);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
     });
-    responseBody = await response.json();
-  });
-
-  test("SHOULD return 201 with an array with more than one element at first", () => {
-    expect(response.status).toBe(201);
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
-  });
-
-  test("SHOULD return 200 with a empty array from second request", () => {
-    expect(response.status).toBe(200);
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBe(0);
   });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,41 +3,31 @@
  */
 import orchestrator from "tests/integration/orchestrator.js";
 
-describe("GET to /api/v1/status", () => {
-  let response;
-  let responseBody;
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    let response;
+    let responseBody;
 
-  beforeAll(async () => {
-    await orchestrator.waitForAllServices();
-  });
+    beforeAll(async () => {
+      await orchestrator.waitForAllServices();
+    });
 
-  beforeEach(async () => {
-    response = await fetch("http://localhost:3000/api/v1/status");
-    responseBody = await response.json();
-  });
+    beforeEach(async () => {
+      response = await fetch("http://localhost:3000/api/v1/status");
+      responseBody = await response.json();
+    });
 
-  test("SHOULD return 200", () => {
-    expect(response.status).toBe(200);
-  });
+    test("Retrieving current system status", () => {
+      expect(response.status).toBe(200);
 
-  test("SHOULD return correct updated_at", async () => {
-    const receivedUpdatedAt = responseBody.updated_at;
-    const parsedUpdateAt = new Date(receivedUpdatedAt).toISOString();
-    expect(receivedUpdatedAt).toBe(parsedUpdateAt);
-  });
+      const receivedUpdatedAt = responseBody.updated_at;
+      const parsedUpdateAt = new Date(receivedUpdatedAt).toISOString();
+      expect(receivedUpdatedAt).toBe(parsedUpdateAt);
 
-  test("SHOULD return postgres version", async () => {
-    const { database } = responseBody.dependencies;
-    expect(database.version).toBe("16.0");
-  });
-
-  test("SHOULD return max connections", () => {
-    const { database } = responseBody.dependencies;
-    expect(database.max_connections).toBe(100);
-  });
-
-  test("SHOULD return used connections", () => {
-    const { database } = responseBody.dependencies;
-    expect(database.opened_connections).toBe(1);
+      const {database} = responseBody.dependencies;
+      expect(database.version).toBe("16.0");
+      expect(database.max_connections).toBe(100);
+      expect(database.opened_connections).toBe(1);
+    });
   });
 });
