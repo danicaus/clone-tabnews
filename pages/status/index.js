ae4ee0b5feea60db98fe8e177f98830220258d6f
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2026-02-24 00:38:54 +0000

    refactor: reorganiza componentes de statusPage

--- pages/status/index.js
+++ pages/status/index.js
@@ -6,35 +6,28 @@ async function fetchStatus() {
   return responseData;
 }
 
+const Loading = () => <span>Carregando...</span>;
+
 function LastUpdated({ dateUpdated }) {
-  return (
-    <p>
-      <b>Última atualização: </b>{" "}
-      {new Date(dateUpdated).toLocaleString("pt-BR")}
-    </p>
-  );
+  return <>{new Date(dateUpdated).toLocaleString("pt-BR")}</>;
 }
 
 function DatabaseStatus({ databaseData }) {
   const { max_connections, opened_connections, version } = databaseData;
-
   return (
-    <>
-      <h2>Banco de dados</h2>
-      <p>
-        <span>
-          <b>Versão: </b> {version}
-        </span>
-        <br />
-        <span>
-          <b>Conexões abertas: </b> {opened_connections}
-        </span>
-        <br />
-        <span>
-          <b>Máximo de conexões: </b> {max_connections}
-        </span>
-      </p>
-    </>
+    <div>
+      <span>
+        <b>Versão: </b> {version}
+      </span>
+      <br />
+      <span>
+        <b>Conexões abertas: </b> {opened_connections}
+      </span>
+      <br />
+      <span>
+        <b>Máximo de conexões: </b> {max_connections}
+      </span>
+    </div>
   );
 }
 
@@ -46,12 +39,20 @@ export default function StatusPage() {
   return (
     <main>
       <h1>Status</h1>
+      <div>
+        <b>Última atualização: </b>{" "}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <LastUpdated dateUpdated={data?.updated_at} />
+        )}
+      </div>
 
-      {!isLoading && data && (
-        <>
-          <DatabaseStatus databaseData={data.dependencies.database} />
-          <LastUpdated dateUpdated={data.updated_at} />
-        </>
+      <h2>Database</h2>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <DatabaseStatus databaseData={data?.dependencies.database} />
       )}
     </main>
   );
